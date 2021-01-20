dca2d0ad4ed6c49a7f248213c01bafb3732b8f5b
maejinkim <>
2021-01-21 00:36:30 +0900

    #5 컨트롤러 read 메소드 어노테이션 수정
    수정 페이지 이동을 위해 어노테이션에 /modify 추가

--- guestbook/src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -49,7 +49,7 @@ public class GuestbookController {
         return "redirect:/guestbook/list";
     }
 
-    @GetMapping("/read")
+    @GetMapping({"/read", "/modify"})
     public void read(long gno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model ){
 
         log.info("gno: " + gno);
