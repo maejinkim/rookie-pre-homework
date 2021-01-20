16a8a21ea84de5c737b1c705a98b0ed3986193b2
maejinkim <>
2021-01-20 18:25:46 +0900

    #2 페이지 처리 테스트

--- guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
+++ guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
@@ -27,15 +27,24 @@ public class GuestbookServiceTests {
         System.out.println(service.register(guestbookDTO));
     }
 
-        @Test
+    @Test
     public void testList(){
 
         PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
 
         PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
 
+        System.out.println("PREV: "+resultDTO.isPrev());
+        System.out.println("NEXT: "+resultDTO.isNext());
+        System.out.println("TOTAL: " + resultDTO.getTotalPage());
+
+        System.out.println("-------------------------------------");
         for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
             System.out.println(guestbookDTO);
         }
+
+        System.out.println("========================================");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
     }
+
 }
