56b83c04e5f0f053106f12cfa4a2a8b9e2be63ef
maejinkim <>
2021-01-20 23:46:27 +0900

    #3 Controller에 등록을 위한 메소드 추가 구현
    컨트롤러에 방명록 등록을 위한 register 메소드를 구현한다.
    get을 통해 등록 화면을 띄우고
    post를 통해 게시물을 등록한 뒤 목록 화면으로 돌아간다.
    redirectAttributes.addFlashAttribute는 msg를 한번만 전달하기 위한 용도로, 모달을 띄워 결과를 보여준다.

--- guestbook/src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -5,7 +5,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.dto.PageRequestDTO;
 import org.zerock.guestbook.service.GuestbookService;
 
@@ -30,4 +33,18 @@ public class GuestbookController {
         model.addAttribute("result", service.getList(pageRequestDTO));
     }
 
+    @GetMapping("/register")
+    public void register(){
+        log.info("regiser get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+        log.info("dto..." + dto);
+
+        Long gno = service.register(dto);
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
+    }
 }
