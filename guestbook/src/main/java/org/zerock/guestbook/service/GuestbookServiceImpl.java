0473fa793ae266ea4a1116da8bf39c0522d6af51
maejinkim <>
2021-01-21 00:07:04 +0900

    #4 Service에 read 메소드 구현
    레포지토리를 통해 엔티티 객체를 가져와 DTO객체로 전달한다.

--- guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -12,6 +12,7 @@ import org.zerock.guestbook.dto.PageResultDTO;
 import org.zerock.guestbook.entity.Guestbook;
 import org.zerock.guestbook.repository.GuestbookRepository;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 @Service
@@ -42,5 +43,13 @@ public class GuestbookServiceImpl implements GuestbookService {
         Page<Guestbook> result = repository.findAll(pageable);
         Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
         return new PageResultDTO<>(result, fn );
+    }
+
+    @Override
+    public GuestbookDTO read(Long gno) {
+
+        Optional<Guestbook> result = repository.findById(gno);
+
+        return result.isPresent()? entityToDto(result.get()): null;
     }
 }
