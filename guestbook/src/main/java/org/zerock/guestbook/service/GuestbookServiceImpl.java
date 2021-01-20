d79fc78c0d9c8e6c2c5e9d235aec32181a9b7cef
maejinkim <>
2021-01-20 17:50:36 +0900

    #2 Service 처리 내용 데이터베이스에 반영
    repository의 save를 이용해 실제 데이터 베이스에 entity를 추가한다.

--- guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -1,13 +1,18 @@
 package org.zerock.guestbook.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.repository.GuestbookRepository;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookServiceImpl implements GuestbookService {
+
+    private final GuestbookRepository repository;
 
     @Override
     public Long register(GuestbookDTO dto) {
@@ -16,9 +21,9 @@ public class GuestbookServiceImpl implements GuestbookService {
         log.info(dto);
 
         Guestbook entity = dtoToEntity(dto);
-
         log.info(entity);
 
-        return null;
+        repository.save(entity);
+        return entity.getGno();
     }
 }
