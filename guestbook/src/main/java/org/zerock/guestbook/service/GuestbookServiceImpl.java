b46ecdc0647774f551169358e313d41f8a93fd37
maejinkim <>
2021-01-20 18:12:17 +0900

    #2 서비스 getList 메소드 테스트

--- guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -2,10 +2,17 @@ package org.zerock.guestbook.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
 import org.zerock.guestbook.entity.Guestbook;
 import org.zerock.guestbook.repository.GuestbookRepository;
+
+import java.util.function.Function;
 
 @Service
 @Log4j2
@@ -13,6 +20,7 @@ import org.zerock.guestbook.repository.GuestbookRepository;
 public class GuestbookServiceImpl implements GuestbookService {
 
     private final GuestbookRepository repository;
+
 
     @Override
     public Long register(GuestbookDTO dto) {
@@ -25,5 +33,14 @@ public class GuestbookServiceImpl implements GuestbookService {
 
         repository.save(entity);
         return entity.getGno();
+    }
+
+    @Override
+    public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
+
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+        Page<Guestbook> result = repository.findAll(pageable);
+        Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
+        return new PageResultDTO<>(result, fn );
     }
 }
