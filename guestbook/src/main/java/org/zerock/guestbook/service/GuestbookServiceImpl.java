e7b3444414ab9831cb6a1d1070c14ca7fd633115
maejinkim <>
2021-01-20 17:29:03 +0900

    #2 dtoToEntity 메소드 구현
    dto를 entity로 변환해주는 메소드를 구현한다.
    인터페이스에 default 키워드를 이용하여 추상 클래스를 생략한다.

--- guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -3,6 +3,7 @@ package org.zerock.guestbook.service;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.entity.Guestbook;
 
 @Service
 @Log4j2
@@ -10,6 +11,14 @@ public class GuestbookServiceImpl implements GuestbookService {
 
     @Override
     public Long register(GuestbookDTO dto) {
+
+        log.info("DTO------------------------");
+        log.info(dto);
+
+        Guestbook entity = dtoToEntity(dto);
+
+        log.info(entity);
+
         return null;
     }
 }
