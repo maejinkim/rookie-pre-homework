18fcf10b4f2a5c0b567aee1400983bc2275420d2
maejinkim <>
2021-01-20 16:37:53 +0900

    #6 데이터 생성 테스트
    데이터베이스에 테스트 데이터를 추가하는 테스트 코드를 작성한다.

---
+++ guestbook/src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
@@ -0,0 +1,28 @@
+package org.zerock.guestbook.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.guestbook.entity.Guestbook;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class GuestbookRepositoryTests {
+
+    @Autowired
+    private GuestbookRepository guestbookRepository;
+
+    @Test
+    public void insertDummies(){
+        IntStream.rangeClosed(1,300).forEach(i -> {
+
+            Guestbook guestbook = Guestbook.builder()
+                    .title("Title...." + i)
+                    .content("Content..." +i)
+                    .writer("user" + (i % 10))
+                    .build();
+            System.out.println(guestbookRepository.save(guestbook));
+        });
+    }
+}
