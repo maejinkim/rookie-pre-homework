316136680905bc32d9faee0e53fc4d943aa493d5
maejinkim <>
2021-01-20 16:43:41 +0900

    #6 데이터 수정 테스트

--- guestbook/src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
+++ guestbook/src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.guestbook.entity.Guestbook;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -24,5 +25,19 @@ public class GuestbookRepositoryTests {
                     .build();
             System.out.println(guestbookRepository.save(guestbook));
         });
+    }
+
+    @Test
+    public void updateTest() {
+        Optional<Guestbook> result = guestbookRepository.findById(300L);
+
+        if(result.isPresent()){
+            Guestbook guestbook = result.get();
+
+            guestbook.changeTitle("Changed Title....");
+            guestbook.changeContent("Changed Content...");
+
+            guestbookRepository.save(guestbook);
+        }
     }
 }
