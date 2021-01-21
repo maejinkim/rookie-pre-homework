bd8616f8ba989c8a797c7f259fc46bab70d35e63
maejinkim <>
2021-01-21 16:34:10 +0900

    #17 게시글 조회 기능 구현 및 화면 처리

--- board/src/main/java/com/zerock/board/controller/BoardController.java
+++ board/src/main/java/com/zerock/board/controller/BoardController.java
@@ -9,6 +9,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -46,5 +47,17 @@ public class BoardController {
 
         return "redirect:/board/list";
     }
-    
+
+    @GetMapping("/read")
+    public void read(@ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO, Long bno, Model model){
+
+        log.info("bno: " + bno);
+
+        BoardDTO boardDTO = boardService.get(bno);
+
+        log.info(boardDTO);
+
+        model.addAttribute("dto", boardDTO);
+
+    }
 }
