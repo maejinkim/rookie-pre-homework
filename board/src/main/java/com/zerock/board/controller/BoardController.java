295b8705c7f9007b70df1b5373a77a3c6f17c65b
maejinkim <>
2021-01-21 16:26:11 +0900

    #17 목록 기능 및 화면 처리 구현

--- board/src/main/java/com/zerock/board/controller/BoardController.java
+++ board/src/main/java/com/zerock/board/controller/BoardController.java
@@ -1,9 +1,13 @@
 package com.zerock.board.controller;
 
 
+import com.zerock.board.dto.PageRequestDTO;
+import com.zerock.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,4 +15,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Log4j2
 @RequiredArgsConstructor
 public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        log.info("list........" + pageRequestDTO);
+
+        model.addAttribute("result", boardService.getList(pageRequestDTO));
+    }
+
 }
