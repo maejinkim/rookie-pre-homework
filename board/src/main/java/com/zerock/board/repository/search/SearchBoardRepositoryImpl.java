74b36e64670fde4628208aa3497dae2eee6564b2
maejinkim <>
2021-01-21 17:17:29 +0900

    #17 JPQL 및 튜플 테스트

--- board/src/main/java/com/zerock/board/repository/search/SearchBoardRepositoryImpl.java
+++ board/src/main/java/com/zerock/board/repository/search/SearchBoardRepositoryImpl.java
@@ -1,8 +1,15 @@
 package com.zerock.board.repository.search;
 
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.JPQLQuery;
 import com.zerock.board.entity.Board;
+import com.zerock.board.entity.QBoard;
+import com.zerock.board.entity.QMember;
+import com.zerock.board.entity.QReply;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
 
 @Log4j2
 public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport implements SearchBoardRepository {
@@ -13,7 +20,29 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
 
     @Override
     public Board search1() {
+
         log.info("search1............");
+
+        QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+        QMember member = QMember.member;
+
+        JPQLQuery<Board> jpqlQuery = from(board);
+        jpqlQuery.leftJoin(member).on(board.writer.eq(member));
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
+
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member.email, reply.count());
+        tuple.groupBy(board);
+
+        log.info("---------------------------");
+        log.info(tuple);
+        log.info("---------------------------");
+
+        List<Tuple> result = tuple.fetch();
+
+        log.info(result);
+
         return null;
     }
+
 }
