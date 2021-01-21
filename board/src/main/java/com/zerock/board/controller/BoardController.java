aa5280a54e5429be7834420a03d12d75bc68a720
maejinkim <>
2021-01-21 16:12:15 +0900

    #17 컨트롤러 생성

---
+++ board/src/main/java/com/zerock/board/controller/BoardController.java
@@ -0,0 +1,14 @@
+package com.zerock.board.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board/")
+@Log4j2
+@RequiredArgsConstructor
+public class BoardController {
+}
