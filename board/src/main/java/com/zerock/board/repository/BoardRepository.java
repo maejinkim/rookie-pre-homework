1d582ec19e1bb286b3464a163724b7696db03bf2
maejinkim <>
2021-01-21 14:17:38 +0900

    #17 목록 화면에서 필요한 JPQL 구성
    보드를 중심으로 해당 게시물에 댓글이 몇개있는지 확인

--- board/src/main/java/com/zerock/board/repository/BoardRepository.java
+++ board/src/main/java/com/zerock/board/repository/BoardRepository.java
@@ -1,6 +1,8 @@
 package com.zerock.board.repository;
 
 import com.zerock.board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +16,12 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     @Query("SELECT b, r FROM Board b LEFT JOIN Reply r ON r.board = b WHERE b.bno = :bno")
     List<Object[]> getBoardWithReply(@Param("bno") Long bno);
+
+    @Query(value ="SELECT b, w, count(r) " +
+            " FROM Board b " +
+            " LEFT JOIN b.writer w " +
+            " LEFT JOIN Reply r ON r.board = b " +
+            " GROUP BY b",
+            countQuery ="SELECT count(b) FROM Board b")
+    Page<Object[]> getBoardWithReplyCount(Pageable pageable);
 }
--- board/src/test/java/com/zerock/board/repository/BoardRepositoryTests.java
+++ board/src/test/java/com/zerock/board/repository/BoardRepositoryTests.java
@@ -6,6 +6,10 @@ import com.zerock.board.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
@@ -71,6 +75,21 @@ public class BoardRepositoryTests {
         for (Object[] arr : result) {
             System.out.println(Arrays.toString(arr));
         }
+    }
+
+    @Test
+    public void testWithReplyCount(){
+
+        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+
+        result.get().forEach(row -> {
+
+            Object[] arr = (Object[])row;
+
+            System.out.println(Arrays.toString(arr));
+        });
     }
 
 }
