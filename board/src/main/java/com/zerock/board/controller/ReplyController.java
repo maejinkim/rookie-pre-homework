b207fb5a817d697cc8a8415bd9b16d9a7a5ca926
maejinkim <>
2021-01-22 14:42:42 +0900

    #17 ReplyCount버튼 클릭시 댓글 목록 기능 및 화면 처리

---
+++ board/src/main/java/com/zerock/board/controller/ReplyController.java
@@ -0,0 +1,34 @@
+package com.zerock.board.controller;
+
+import com.zerock.board.dto.ReplyDTO;
+import com.zerock.board.service.ReplyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/replies/")
+@Log4j2
+@RequiredArgsConstructor
+public class ReplyController {
+
+    private final ReplyService replyService;
+
+
+    @GetMapping(value = "/board/{bno}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ReplyDTO>> getListByBoard(@PathVariable("bno") Long bno) {
+        log.info("bno: "+bno);
+
+        return new ResponseEntity<>(replyService.getList(bno), HttpStatus.OK);
+    }
+
+
+}
