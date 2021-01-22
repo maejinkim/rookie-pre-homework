d9a132c5fc099b0a405df2459cf71557a86fd292
maejinkim <>
2021-01-22 15:22:16 +0900

    #17 댓글 삭제 기능 구현 및 화면 처리

--- board/src/main/java/com/zerock/board/controller/ReplyController.java
+++ board/src/main/java/com/zerock/board/controller/ReplyController.java
@@ -35,5 +35,13 @@ public class ReplyController {
         return new ResponseEntity<>(rno, HttpStatus.OK);
     }
 
+    @DeleteMapping("/{rno}")
+    public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+        log.info(rno);
+
+        replyService.remove(rno);
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+
 
 }
